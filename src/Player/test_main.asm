947886cce7d4ddb7143992aaf11e84ec74c41218
yu-301 <lyjlyj9531@gmail.com>
2025-12-01 20:09:13 +0800

    Update and rename test_player.asm to test_main.asm

---
+++ src/Player/test_main.asm
@@ -0,0 +1,56 @@
+INCLUDE Irvine32.inc
+
+InputModule PROTO
+PlayerPos PROTO
+PlayerRotate PROTO
+PlayerState PROTO
+PlayerReset PROTO
+
+EXTERN playerX:DWORD, playerY:DWORD, dir:DWORD, inputCode:DWORD
+
+.data
+xMsg  BYTE "X=",0
+yMsg  BYTE "Y=",0
+dirMsg BYTE "DIR=",0
+
+.code
+main PROC
+
+main_loop:
+
+    ; 印 "X="
+    mov edx, OFFSET xMsg
+    call WriteString
+    mov eax, playerX
+    call WriteDec
+
+    call Crlf
+
+    ; 印 "Y="
+    mov edx, OFFSET yMsg
+    call WriteString
+    mov eax, playerY
+    call WriteDec
+
+    call Crlf
+
+    ; 印 "DIR="
+    mov edx, OFFSET dirMsg
+    call WriteString
+    mov eax, dir
+    call WriteDec
+
+    call Crlf
+    call Crlf
+
+    ; 取得輸入
+    call InputModule
+    call PlayerPos
+    call PlayerRotate
+    call PlayerReset
+
+    jmp main_loop
+
+main ENDP
+
+END main
