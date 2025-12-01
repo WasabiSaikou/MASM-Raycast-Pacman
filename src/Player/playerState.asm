00f5ecc9310bf9705c63fd7d5542b97bb8dd6b51
yu-301 <lyjlyj9531@gmail.com>
2025-12-01 19:48:36 +0800

    Update playerState.asm

---
+++ src/Player/playerState.asm
@@ -0,0 +1,30 @@
+TITLE PlayerState
+
+INCLUDE Irvine32.inc
+PUBLIC PlayerState
+PUBLIC playerX, playerY, dir, inputCode
+PUBLIC mapWidth, mapHeight
+
+.data
+    mapWidth  DWORD 7
+    mapHeight DWORD 7
+
+    playerX   DWORD 3
+    playerY   DWORD 3
+    dir       DWORD 1        ; 0=↑,1=→,2=↓,3=←
+    inputCode DWORD 0        ; 0=none, 1~7
+
+.code
+; ------------------------------------
+; PlayerState
+; 回傳 (X,Y,Dir)
+; eax = X, ebx = Y, ecx = dir
+; ------------------------------------
+PlayerState PROC
+    mov eax, playerX
+    mov ebx, playerY
+    mov ecx, dir
+    ret
+PlayerState ENDP
+
+END
