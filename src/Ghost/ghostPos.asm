31e3119a183fa56721bd283ab693fbd4fdbf3a52
HeLLoHowAreUm <0125ycy@gmail.com>
2025-12-07 11:26:24 +0800

    Update ghostPos.asm

--- src/Ghost/ghostPos.asm
+++ src/Ghost/ghostPos.asm
@@ -1,111 +1,12 @@
-TITLE PlayerPos
 
-INCLUDE Irvine32.inc
+INCLUDE AIdataStructure.asm
 
-EXTERN inputCode:DWORD
-EXTERN playerX:DWORD, playerY:DWORD, dir:DWORD
-EXTERN N:DWORD
+.CODE
 
-PUBLIC PlayerPos
+Get_Ghost_Position PROC NEAR
+     MOV AX, GHOST_POS_X
+     MOV BX, GHOST_POS_Y
+     RET
+Get_Ghost_Position ENDP
 
-.code
-PlayerPos PROC
-    mov eax, inputCode
-    cmp eax, 1
-    je wCase
-    cmp eax, 2
-    je aCase
-    cmp eax, 3
-    je sCase
-    cmp eax, 4
-    je dCase
-    ret
-
-;--------------- W ---------------
-wCase:
-    mov eax, dir
-    cmp eax, 0
-    je up
-    cmp eax, 1
-    je right
-    cmp eax, 2
-    je down
-    cmp eax, 3
-    je left
-    ret
-
-;--------------- S ---------------
-sCase:
-    mov eax, dir
-    cmp eax, 0
-    je down
-    cmp eax, 1
-    je left
-    cmp eax, 2
-    je up
-    cmp eax, 3
-    je right
-    ret
-
-;--------------- A ---------------
-aCase:
-    mov eax, dir
-    cmp eax, 0
-    je left
-    cmp eax, 1
-    je up
-    cmp eax, 2
-    je right
-    cmp eax, 3
-    je down
-    ret
-
-;--------------- D ---------------
-dCase:
-    mov eax, dir
-    cmp eax, 0
-    je right
-    cmp eax, 1
-    je down
-    cmp eax, 2
-    je left
-    cmp eax, 3
-    je up
-    ret
-
-;----------------------------------
-up:
-    mov eax, playerY
-    cmp eax, 1
-    jle pos_end
-    dec playerY
-    jmp pos_end
-
-right:
-    mov eax, playerX
-    mov ecx, N
-    cmp eax, ecx
-    jge pos_end
-    inc playerX
-    jmp pos_end
-
-down:
-    mov eax, playerY
-    mov ecx, N
-    cmp eax, ecx
-    jge pos_end
-    inc playerY
-    jmp pos_end
-
-left:
-    mov eax, playerX
-    cmp eax, 1
-    jle pos_end
-    dec playerX
-    jmp pos_end
-
-pos_end:
-    ret
-
-PlayerPos ENDP
 END
