0756631ba70f1805e07239f0fcaee45b59fab567
yu-301 <lyjlyj9531@gmail.com>
2025-12-07 14:57:03 +0800

    Update InputModule.asm

--- src/Player/InputModule.asm
+++ src/Player/InputModule.asm
@@ -3,14 +3,17 @@ TITLE InputModule
 INCLUDE Irvine32.inc
 
 EXTERN inputCode:DWORD
+EXTERN moveState:DWORD
 
 PUBLIC InputModule
 
 .code
 InputModule PROC
 
-    call ReadChar
+    mov eax, dword ptr moveState
     mov dword ptr inputCode, 0
+
+    call ReadKey
 
     ; Reset (R/r)
     cmp al, 'R'
@@ -44,19 +47,23 @@ InputModule PROC
     cmp  ax, 4D00h                 ; right arrow
     je   setRotateRight
 
-    ret
+    jmp input_end
 
 setForward:      
     mov inputCode, 1
+    mov moveState, 1
     ret
 setLeft:
     mov inputCode, 2
+    mov moveState, 2
     ret
 setBackward:
     mov inputCode, 3
+    mov moveState, 3
     ret
 setRight:
     mov inputCode, 4
+    mov moveState, 4
     ret
 setRotateLeft:   
     mov  inputCode, 5
@@ -66,8 +73,11 @@ setRotateRight:
     ret
 setReset:        
     mov  inputCode, 7
+    mov  moveState, 0
+    ret
+
+input_end:
     ret
 
 InputModule ENDP
 END
-
