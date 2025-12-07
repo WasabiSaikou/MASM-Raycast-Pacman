469afe5ebd52c92ac8ea6394fd6ec2fbdefcc5af
yu-301 <lyjlyj9531@gmail.com>
2025-12-07 20:19:53 +0800

    Update playerState.asm

--- src/Player/playerState.asm
+++ src/Player/playerState.asm
@@ -4,6 +4,7 @@ INCLUDE Irvine32.inc
 PUBLIC PlayerState
 PUBLIC playerX, playerY, dir, inputCode
 PUBLIC moveState
+PUBLIC prevX, prevY
 
 EXTERN N:DWORD
 
@@ -13,6 +14,8 @@ EXTERN N:DWORD
     dir       DWORD 1        ; 0=↑,1=→,2=↓,3=←
     inputCode DWORD 0        ; 0=none, 1~7
     moveState DWORD 0        ; 0 = stop, 1 = frond, 2 = left, 3 = behind, 4 = right
+    prevX     DWORD 16
+    prevY     DWORD 15
 
 .code
 ; ------------------------------------
