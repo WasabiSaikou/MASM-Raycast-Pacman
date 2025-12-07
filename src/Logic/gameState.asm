98284afc203d1849398a13d05a9d01e00d3009fa
wyx-2025 <sandy.wu113502504@g.ncu.edu.tw>
2025-12-07 22:10:44 +0800

    Update gameState.asm

--- src/Logic/gameState.asm
+++ src/Logic/gameState.asm
@@ -1,17 +1,74 @@
 TITLE gameState
-.386
-.MODEL FLAT, C
+INCLUDE Irvine32.inc
 
+PUBLIC point, gameOverFlag, gameWinFlag
 PUBLIC gameState
 
-EXTERN playerState:PROC
-EXTERN ghostPos:PROC
+EXTERN playerX:DWORD, playerY:DWORD
+EXTERN prevX:DWORD, prevY:DWORD
+EXTERN ghostX:DWORD, ghostY:DWORD
+
+EXTERN resetMaze:PROC
+
+.data
+point DWORD 0
+gameOverMsg BYTE "GAME OVER",0
+gameWinMsg BYTE "YOU WIN", 0
+gameOverFlag DWORD 0
+gameWinFlag DWORD 0
 
 .code
-gameState PROC 
+gameState PROC
+    ; check if win the game
+    mov eax, gameWinFlag
+    cmp eax, 1
+    je Win
+
+    ; check if game over
+    mov eax, gameOverFlag
+    cmp eax, 1
+    jne continueGame
+    jmp GameOver
+
+Win:
+    mov edx, OFFSET gameWinMsg
+    call WriteString
+    call Crlf
+    jmp delay
+
+GameOver:
+    ; 顯示 GameOver 訊息
+    mov edx, OFFSET gameOverMsg
+    call WriteString
+    call Crlf
+
+delay:
+    ; 暫停 3 秒
+    mov eax, 3000
+    call Delay
+
+resetGame:
+    xor eax, eax
+    mov gameOverFlag, eax
+    mov gameWinFlag, eax
+    mov point, eax
+
+    mov eax, 16
+    mov playerX, eax
+    mov prevX, eax
+    mov eax, 15
+    mov playerY, eax
+    mov prevY, eax
     
+    mov eax, 27
+    mov ghostX, eax
+    mov eax, 5
+    mov ghostY, eax
+    
+    call resetMaze
 
-
+continueGame:
+    ret
 
 gameState ENDP
 END
