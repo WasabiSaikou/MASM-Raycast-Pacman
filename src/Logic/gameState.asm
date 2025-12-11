0a42a1c325eb278b2383d942f663eb950c323cf1
wyx-2025 <sandy.wu113502504@g.ncu.edu.tw>
2025-12-11 14:43:38 +0800

    update gameState

--- src/Logic/gameState.asm
+++ src/Logic/gameState.asm
@@ -1,72 +1,81 @@
-    TITLE gameState
+TITLE gameState
 
-    INCLUDE Irvine32.inc
+INCLUDE Irvine32.inc
 
-    PUBLIC point, gameOverFlag, gameWinFlag
-    PUBLIC gameState
+PUBLIC point, gameOverFlag, gameWinFlag
+PUBLIC gameState
 
-    EXTERN playerX:DWORD, playerY:DWORD, dir:DWORD
-    EXTERN prevX:DWORD, prevY:DWORD
-    EXTERN ghostX:DWORD, ghostY:DWORD
-    EXTERN inputCode:DWORD
+EXTERN playerX:DWORD, playerY:DWORD, dir:DWORD
+EXTERN prevX:DWORD, prevY:DWORD
+EXTERN ghostX:DWORD, ghostY:DWORD
+EXTERN inputCode:DWORD
 
-    EXTERN resetMaze:PROC
-    EXTERN PlayerReset:PROC
+EXTERN resetMaze:PROC
+EXTERN PlayerReset:PROC
 
-    .data
-    point DWORD 0
-    gameOverMsg BYTE "GAME OVER",0
-    gameWinMsg BYTE "YOU WIN", 0
-    gameOverFlag DWORD 0
-    gameWinFlag DWORD 0
+.data
+point DWORD 0
+gameOverMsg BYTE "GAME OVER",0
+gameWinMsg BYTE "YOU WIN", 0
+pressKeyMsg BYTE "Press any key to reset...", 0
+gameOverFlag DWORD 0
+gameWinFlag DWORD 0
 
-    .code
-    gameState PROC
-        ; check if win the game
-        mov eax, gameWinFlag
-        cmp eax, 1
-        je Win
+.code
+gameState PROC
+    ; check if win the game
+    mov eax, gameWinFlag
+    cmp eax, 1
+    je Win
 
-        ; check if game over
-        mov eax, gameOverFlag
-        cmp eax, 1
-        jne continueGame
-        jmp GameOver
+    ; check if game over
+    mov eax, gameOverFlag
+    cmp eax, 1
+    jne continueGame
+    jmp GameOver
 
-    Win:
-        mov edx, OFFSET gameWinMsg
-        call WriteString
-        call Crlf
-        jmp timeInterval
+Win:
+    ; show the message of win
+    mov edx, OFFSET gameWinMsg
+    call WriteString
+    call Crlf
+    jmp timeInterval
 
-    GameOver:
-        ; 顯示 GameOver 訊息
-        mov edx, OFFSET gameOverMsg
-        call WriteString
-        call Crlf
+GameOver:
+    ; show the message of GameOver 
+    mov edx, OFFSET gameOverMsg
+    call WriteString
+    call Crlf
 
-    timeInterval:
-        ; 暫停 3 秒
-        
+timeInterval:
+    ; wait for 1 seconds
+    mov ecx, 1000
+    call Delay
 
+    ; press any key to restart
+    mov edx, OFFSET pressKeyMsg
+    call WriteString
+    call Crlf
 
-    resetGame:
-        xor eax, eax
-        mov gameOverFlag, eax
-        mov gameWinFlag, eax
-        mov point, eax
+    call ReadChar
 
-        call PlayerReset
-        
-        mov eax, 26
-        mov ghostX, eax
-        mov eax, 4
-        mov ghostY, eax
-        
-        call resetMaze
+resetGame:
+    xor eax, eax
+    mov gameOverFlag, eax
+    mov gameWinFlag, eax
+    mov point, eax
 
-    continueGame:
-        ret
+    call PlayerReset
+    
+    mov eax, 26
+    mov ghostX, eax
+    mov eax, 4
+    mov ghostY, eax
+    
+    call resetMaze
 
-    gameState ENDP
-    END
+continueGame:
+    ret
+
+gameState ENDP
+END
