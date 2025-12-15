6e49920920dba083ccdaa90a78035df5f53d347f
WasabiSaikou <65612399+WasabiSaikou@users.noreply.github.com>
2025-12-15 11:54:44 +0800

    Update renderGL.c

--- src/Interface/renderGL.c
+++ src/Interface/renderGL.c
@@ -226,8 +226,7 @@ int __cdecl InitOpenGL(void) {
     glDisable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    
-    // Build bitmap font
+
     BuildFont(hDC);
     
     return 1;
